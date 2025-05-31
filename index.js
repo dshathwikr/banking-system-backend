7a5c33b8e1bf1073c28eb106eb24025f22d8f183
Shathwik Reddy Duvvuru <dshathwikr@gmail.com>
2025-05-31 10:59:41 +0530

    Added cors

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ const express = require("express");
 const connectDB = require("./config/db");
 const accountRoutes = require("./routes/accountRoutes");
 const dotenv = require("dotenv");
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 connectDB();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", accountRoutes);
 
